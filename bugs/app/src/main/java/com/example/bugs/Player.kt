a5dae349903ab2eb42b74193b81734cf05d6e937
danyowoj <latsuk1803@gmail.com>
2025-09-09 14:33:25 +0700

    Задание 2. Добавил класс игрока.

---
+++ bugs/app/src/main/java/com/example/bugs/Player.kt
@@ -0,0 +1,10 @@
+package com.example.bugs
+
+data class Player(
+    val name: String,
+    val gender: String,
+    val course: String,
+    val difficulty: Int,
+    val birthDate: String,
+    val zodiac: String
+)
